96cf30c4e584050af8d15f10d9f94b1dcbbcf0ed
Ema Nichole Westerfeld <ewesterf@purdue.edu>
2024-01-17 12:44:49 -0500

    Save2

--- labtwo/labtwo.cpp
+++ labtwo/labtwo.cpp
@@ -10,16 +10,21 @@ int main()
     float A;
     float B;
     float X;
+    cout << "Hello, my name is Ema and I'm going to solve the equation:" << endl;
     cout << "Ax + B = 0" << endl;
+    cout << "For x" << endl;
+    cout << endl;
+
     cout << "Please enter a value for A:";
     cin >> A;
     cout << "Please enter a value for B:";
     cin >> B;
+    cout << endl;
 
-    cout << "A=" << A << endl;
-    cout << "B=" << B << endl;
+    cout << "Solving " << A << "x + " << B << " = 0 for x..." << endl;
 
     X = -B / A;
+    cout << "The answer is:" << endl;
     cout << "x=" << X << endl;
 }
 
